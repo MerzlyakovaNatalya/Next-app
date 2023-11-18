3636e2e4e8fbaa0cdcc73e0f3e4434b99158c141
MerzlyakovaNatalya <m.artem.v@inbox.ru>
2023-11-18 19:31:07 +0300

    add change 18

--- components/signin_form/index.tsx
+++ components/signin_form/index.tsx
@@ -1,36 +1,36 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import { signIn } from 'next-auth/react'
-import type { FormEventHandler } from 'react'
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import type { FormEventHandler } from "react";
+import styles from './page.module.scss'
 
 const SignInForm = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault()
-     
-    const formData = new FormData(event.currentTarget)
+    event.preventDefault();
 
-    const res = await signIn('credential' , {
-      email: formData.get('email'),
-      password: formData.get('password'),
-      redirect: false
-    })
+    const formData = new FormData(event.currentTarget);
 
-    if(res && !res.error) {
-      router.push('/profile')
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+    if (res && !res.error) {
+      router.push("/profile");
     }
-  }
+  };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" ></input>
-        <input type="password" name="password" required></input>
+    <div className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form_wrap}>
+        <input className={styles.form_input} type="email" name="email" required></input>
+        <input className={styles.form_input} type="password" name="password" required></input>
         <button type="submit">Отправить</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignInForm
+export default SignInForm;
